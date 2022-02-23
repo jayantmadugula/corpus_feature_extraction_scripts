919d87ad88b58eabe638502007f2b6b8c10861c7
Jayant Madugula <jayant.madugula@gmail.com>
2022-02-22 23:25:22 -0800

    Added test for Pipeline class

--- tests/pipeline_test.py
+++ tests/pipeline_test.py
@@ -17,11 +17,11 @@ class PipelineTests(unittest.TestCase):
         return super().setUp()
     
     @staticmethod
-    def extraction_fn(df: pd.DataFrame) -> pd.DataFrame:
+    def simple_extraction_fn(df: pd.DataFrame) -> pd.DataFrame:
         return df
 
     # Test functions
-    def test_basic_pipeline(self):
+    def test_standard_configuration(self):
         pre_extraction_fns = [
             lambda x: x + 1,
             lambda x: x * 2
@@ -38,8 +38,27 @@ class PipelineTests(unittest.TestCase):
         p = Pipeline(
             data_save_fn=save_fn,
             pre_extraction_fns=pre_extraction_fns,
-            feature_extraction_fn=PipelineTests.extraction_fn,
+            feature_extraction_fn=PipelineTests.simple_extraction_fn,
             post_extraction_fns=post_extraction_fns,
             batch_size=batch_size
         )
         p.start([self.test_df.copy(deep=True)])
+
+    def test_no_processing_fns(self):
+        pre_extraction_fns = []
+        post_extraction_fns = []
+        batch_size = 3
+
+        def save_fn(df: pd.DataFrame):
+            assert(df.shape[1] == self.test_df.shape[1])
+            assert(df.shape[0] == self.test_df.shape[0])
+        
+        p = Pipeline(
+            data_save_fn=save_fn,
+            pre_extraction_fns=pre_extraction_fns,
+            feature_extraction_fn=PipelineTests.simple_extraction_fn,
+            post_extraction_fns=post_extraction_fns,
+            batch_size=batch_size
+        )
+        p.start([self.test_df.copy(deep=True)])
+            
