b6e2123497bbbad16c0288a5b14e75b42b8af905
Jayant Madugula <jayant.madugula@gmail.com>
2022-03-02 00:14:08 -0800

    Added unit tests for text preprocessing functions

---
+++ tests/text_preprocessing_test.py
@@ -0,0 +1,46 @@
+import unittest
+from processing_functions import text_preprocessing
+import pandas as pd
+
+class TextPreprocessingTests(unittest.TestCase):
+    def test_lowercase(self):
+        test_df = pd.Series([
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+            "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+            "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
+        ])
+        result = text_preprocessing.lowercase_words(test_df)
+        expected = pd.Series([
+            "lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+            "ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+            "duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
+        ])
+        assert((result == expected).all())
+
+    def test_remove_punctuation(self):
+        test_df = pd.Series([
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+            "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+            "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
+        ])
+        result = text_preprocessing.remove_punctuation(test_df)
+        expected = pd.Series([
+            "Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+            "Ut enim ad minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat",
+            "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur"
+        ])
+        assert((result == expected).all())
+
+    def test_normalized_spacing(self):
+        test_df = pd.Series([
+            "Lorem ipsum dolor sit amet, consectetur    adipiscing elit, sed  do eiusmod tempor incididunt   ut labore et dolore magna aliqua.",
+            "Ut enim ad minim veniam, quis   nostrud exercitation ullamco laboris    nisi ut aliquip ex ea commodo consequat.",
+            "Duis aute   irure dolor in    reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
+        ])
+        result = text_preprocessing.normalize_spacing(test_df)
+        expected = pd.Series([
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+            "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+            "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
+        ])
+        assert((result == expected).all())
