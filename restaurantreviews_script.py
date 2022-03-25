ce19430a029a7c5cd5e4902502273eced36f18c7
Jayant Madugula <jayant.madugula@gmail.com>
2022-03-24 23:08:31 -0700

    Integrated parts-of-speech filtering with restaurant reviews script

--- restaurantreviews_script.py
+++ restaurantreviews_script.py
@@ -12,6 +12,7 @@ import argparse
 import json
 import sqlite3
 from functools import partial
+from utilities.input_validation_utilities import validate_spacy_pos
 from utilities.database_utilities import load_df, save_df, remove_existing_table
 from processing_functions import ngram_generation, text_preprocessing as tp
 from pipeline import Pipeline
@@ -31,10 +32,22 @@ if __name__ == '__main__':
         type=int, 
         default=2,
         nargs='?',
-        help='this size of the resulting ngrams will be 2 * N + 1')
+        help='the size of the resulting ngrams will be 2 * N + 1')
+    parser.add_argument(
+        'use_pos_filtering',
+        metavar='P',
+        type=bool,
+        default=False,
+        nargs='?',
+        help='''
+            if set to True, ngram generation will only occur for ngrams centered on words
+            that are defined in parameters.json
+            '''
+    )
 
     args = parser.parse_args()
     window_len =  args.ngram_context_size # len(ngram) = (2 * window_len) + 1
+    use_pos_filtering = args.use_pos_filtering
 
     # Load parameters.
     with open('./parameters.json') as params_fp:
@@ -46,7 +59,11 @@ if __name__ == '__main__':
     database_path = params['restaurant_reviews']['database_path']
     table_name = params['restaurant_reviews']['text_table_name']
 
+    pos_filter = params['restaurant_reviews']['pos_filter_list']
+    validate_spacy_pos(pos_filter)
+    
     output_table_name = f'restaurantreviews_n={window_len}'
+    if use_pos_filtering: output_table_name = f'{output_table_name}_pos-filter'
 
     # Remove pre-existing table if necessary.
     conn = sqlite3.connect(database_path)
@@ -56,20 +73,32 @@ if __name__ == '__main__':
     log_dict = dict()
     log_dict['Pipeline Input'] = {
         'Database Path': database_path,
-        'Table Name': table_name
+        'Table Name': table_name,
+        'Include PoS Filtering': use_pos_filtering
     }
     log_dict['ngram Size'] = f'{window_len}'
 
     log_dict['Pipeline Output'] = {
         'Table Name': output_table_name
     }
+
     run_name = output_table_name
 
     # Get data iterator.
     sql_iter = load_df(conn, table_name, chunksize=batch_size)
 
     # Call Pipeline with data and processing functions.
-    ngram_extraction_fn = partial(ngram_generation.generate_corpus_ngrams, col_name='review_spdocs', n=window_len)
+    if use_pos_filtering:
+        ngram_extraction_fn = partial(
+            ngram_generation.generate_corpus_ngrams, 
+            col_name='review_spdocs', 
+            n=window_len,
+            pos_filter=pos_filter)
+    else:
+        ngram_extraction_fn = partial(
+            ngram_generation.generate_corpus_ngrams, 
+            col_name='review_spdocs', 
+            n=window_len)
     ngram_extraction_fn.__name__ = ngram_generation.generate_corpus_ngrams.__name__
 
     partial_save_fn = partial(save_df, conn=conn, table_name=output_table_name)
